491cdf6a3a19bd99d34c5bb908bf91530a5a9bcd
David <avidmachado@gmail.com>
2024-01-06 13:53:29 -0500

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -4,3 +4,21 @@ var cursor2 = document.querySelector(".cursor2");
 document.addEventListener("mousemove", function(e){
   cursor.style.cssText = cursor2.style.cssText = "left: " + e.clientX + "px; top: " + e.clientY + "px;";
 });
+
+const nav = document.querySelector('.header__links')
+const abrir = document.querySelector('.abrirMenu')
+const cerrar = document.querySelector('.cerrarMenu')
+const link = document.querySelector('.header__link')
+
+abrir.addEventListener('click', () => {
+  nav.classList.remove('invisible')
+})
+
+link.addEventListener('click', () => {
+  nav.classList.remove('invisible')
+  nav.classList.add('invisible')
+})
+
+cerrar.addEventListener('click', () => {
+  nav.classList.add('invisible')
+})
